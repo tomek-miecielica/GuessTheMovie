4776e300ce3ccceced7189ab5d21ea9dd20e910f
Tomek Miecielica <tomek.miecielica@gmail.com>
2020-05-01 17:51:30 +0200

    Unused code removed from Movie class

--- src/Movie.java
+++ src/Movie.java
@@ -5,53 +5,41 @@ public class Movie {
 
     //constructor
 
-    public Movie (String title, int index, char[] movieName){
+    public Movie(String title, int index, char[] movieName) {
         this.title = title;
         this.index = index;
         this.movieName = movieName;
     }
 
     //setters
-    public void setTitle(String movieTitle){
+    public void setTitle(String movieTitle) {
         title = movieTitle;
     }
-    public void setIndex (int ind){
+
+    public void setIndex(int ind) {
         index = ind;
     }
-//    public void setMovieName (int size){
-//        movieName = new char[size - 1]; // because it starts from 0, not from 1
-//    }
+
     //getters
-    public String getTitle(){
+    public String getTitle() {
         return title;
     }
-    public int getIndex(){
+
+    public int getIndex() {
         return index;
     }
-    public int getNumOfLetters(){
+
+    public int getNumOfLetters() {
         return title.length();
     }
-    public char[] getMovieName(){
+
+    public char[] getMovieName() {
         return movieName;
     }
+
     //dodaj error handling by nie byo out of range
-    public char getMovieLetter(int i){
+    public char getMovieLetter(int i) {
         return movieName[i];
     }
-
-
-    public static char[] hideMovieName(char[] array, int size){
-        for (int i = 0; i < size; i++){
-            array[i] = '_';
-        }
-        return array;
-    }
-    public static void printHiddenName (char[] array, int size){
-        System.out.println("Ypu are guessing: ");
-        for (int i = 0; i < size; i++){
-            System.out.print(array[i]);
-        }
-    }
-
 }
 
